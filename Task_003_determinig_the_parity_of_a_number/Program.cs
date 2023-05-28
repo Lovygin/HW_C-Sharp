a234183979a07a3f191de4bd0ba840e57eeb3043
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-05-28 18:40:42 +0300

    problem solved

---
+++ Task_003_determinig_the_parity_of_a_number/Program.cs
@@ -0,0 +1,8 @@
+﻿//determining - определение; the parity - четность
+Console.Clear();
+Console.WriteLine("Please, enter the number");
+int num = int.Parse(Console.ReadLine());
+if(num % 2 == 0){
+    Console.WriteLine(num + " - parity number");}
+else{
+    Console.WriteLine(num + " - not even number");}      //even - четный
