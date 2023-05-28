5f7738d527976bde85cbb0dd10158617eb3c78fa
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-05-28 18:46:35 +0300

    problem solved

---
+++ Task_004_search_all_parity_number_in_the_number_N/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Please, enter number N");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+do// Делай (do) это (if()), пока (while) не выполнилось это.
+{
+    if(i % 2 == 0){
+        Console.WriteLine(i);}    
+    i++;
+}
+while(i <= N);
+return;
