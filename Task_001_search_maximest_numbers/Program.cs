9f03cda360fbd87260763301318124724a33212f
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-05-28 18:51:11 +0300

    problem solved

---
+++ Task_001_search_maximest_numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Please, enter 1-st number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Please, enter 2-nd number");
+int b = int.Parse(Console.ReadLine());
+if(a > b){
+    Console.WriteLine(a + " - maximest number and " + b + " minimest number");}
+else{
+    Console.WriteLine(b + " - maximest number and " + a + " minimest number");}//text conclusion - текстовый вывод или вывод текта
