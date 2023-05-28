1503396957366327008969a8c4243c46e95cfd66
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-05-28 18:36:33 +0300

    problem solved

---
+++ Task_002_serch_maximest_among_three_numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Please, enter 1-st number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Please, enter 2-nd number");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Please, enter 3-rd number");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+        max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("maximest number: " + max);
